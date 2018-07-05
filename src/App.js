11d1d43d0d1bfbb989059de948ff74bcb9cffea5
Maxim Ten <maximten94@gmail.com>
2018-07-05 20:09:29 +0600

    Blur added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Shaders, Node, GLSL } from "gl-react"
-import { Surface } from "gl-react-dom"
+import { Surface } from 'gl-react-dom'
+import Animated from "animated"
 
 const shaders = Shaders.create({
   circle: {
@@ -12,8 +13,8 @@ const shaders = Shaders.create({
       uniform float radius;
       uniform float border;
       uniform float angle;
-      uniform vec4 bgColor;
-      uniform vec4 color;
+      uniform vec3 bgColor;
+      uniform vec3 color;
       float atan2(float y, float x) {
         if (x > 0.0) {
           return atan(y / x);
@@ -28,35 +29,55 @@ const shaders = Shaders.create({
         }
       }
       void main() {
-        float dist = sqrt((uv.x - center.x)*(uv.x - center.x) + (uv.y - center.y)*(uv.y - center.y));
+        float dist = distance(uv, center);
+        float distFromRad = abs(dist - radius);
         if (dist > (radius - border) && dist < (radius + border)) {
           vec2 v1 = uv - center;
           float angleRad = atan2(-v1.x, -v1.y);
           float angleDeg = angleRad * (180.0 / M_PI) + 180.0;
           if (angleDeg < angle) {
-            gl_FragColor = color;
+            gl_FragColor = vec4(mix(color, bgColor, smoothstep(0.0, border, distFromRad)), 1);
           } else {
-            gl_FragColor = bgColor;
+            gl_FragColor = vec4(bgColor, 1);
           }
         } else {
-          gl_FragColor = bgColor;
+          gl_FragColor = vec4(bgColor, 1);
         }
       }`
   }
 })
 
+class Cirlce extends Component {
+  render() {
+    return (
+      <Node shader={shaders.circle} uniforms={{ 
+        center: [0.5, 0.5],
+        radius: 0.4,
+        border: 0.03,
+        bgColor: [1, 0.5, 0.5],
+        color: [0, 0, 0],
+        angle: this.props.angle
+      }} />
+    )
+  }
+}
+
+const AnimatedCircle = Animated.createAnimatedComponent(Cirlce)
+
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      angle: new Animated.Value(0)
+    }
+  }
+  componentDidMount() {
+    Animated.timing(this.state.angle, { toValue: 320, duration: 1000 }).start()
+  }
   render() {
     return (
       <Surface width={600} height={600}>
-        <Node shader={shaders.circle} uniforms={{ 
-          center: [0.5, 0.5],
-          radius: 0.4,
-          border: 0.03,
-          bgColor: [1, 0, 0, 0.3],
-          color: [0, 0, 0, 1],
-          angle: 45.0
-        }} />
+        <AnimatedCircle angle={this.state.angle}/>
       </Surface>
     )
   }
