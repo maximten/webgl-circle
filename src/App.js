fdb637c30aa082014f4b2b767c42090ca001baef
Maxim Ten <maximten94@gmail.com>
2018-07-05 17:08:12 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const shaders = Shaders.create({
       uniform vec4 bgColor;
       uniform vec4 color;
       void main() {
-        float dist = sqrt((uv.x - 0.5)*(uv.x - 0.5) + (uv.y - 0.5)*(uv.y - 0.5));
-        if (dist > 0.35 && dist < 0.4) {
+        float dist = sqrt((uv.x - center.x)*(uv.x - center.x) + (uv.y - center.y)*(uv.y - center.y));
+        if (dist > (radius - border) && dist < (radius + border)) {
           gl_FragColor = vec4(color);
         } else {
           gl_FragColor = vec4(bgColor);
@@ -29,8 +29,8 @@ export default class App extends Component {
       <Surface width={600} height={600}>
         <Node shader={shaders.circle} uniforms={{ 
           center: [0.5, 0.5],
-          radius: 0.1,
-          border: 0.1,
+          radius: 0.4,
+          border: 0.03,
           bgColor: [1, 0, 0, 0.3],
           color: [0, 0, 0, 1],
         }} />
